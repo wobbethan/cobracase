5cdd2810ffcd3f988e6a1f960c3dd760b2545dff
wobbethan <wobbethan@gmail.com>
2024-05-10 00:31:59 -0400

    configuration page

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -1,5 +1,8 @@
 import { createUploadthing, type FileRouter } from "uploadthing/next";
 import { z } from "zod";
+import sharp from "sharp";
+import { db } from "@/db";
+
 const f = createUploadthing();
 
 export const ourFileRouter = {
@@ -10,7 +13,32 @@ export const ourFileRouter = {
     })
     .onUploadComplete(async ({ metadata, file }) => {
       const { configId } = metadata.input;
-      return { configId };
+
+      const res = await fetch(file.url);
+      const buffer = await res.arrayBuffer();
+      const imgMetadata = await sharp(buffer).metadata();
+      const { width, height } = imgMetadata;
+
+      if (!configId) {
+        const configuration = await db.configuartion.create({
+          data: {
+            imageUrl: file.url,
+            height: height || 500,
+            width: width || 500,
+          },
+        });
+        return { configId: configuration.id };
+      } else {
+        const updatedConfiguration = await db.configuartion.update({
+          where: {
+            id: configId,
+          },
+          data: {
+            croppedImageUrl: file.url,
+          },
+        });
+        return { configId: updatedConfiguration.id };
+      }
     }),
 } satisfies FileRouter;
 
---
+++ src/app/configure/design/page.tsx
@@ -0,0 +1,37 @@
+import DesignConfigurator from "@/components/DesignConfigurator";
+import { db } from "@/db";
+import { notFound } from "next/navigation";
+
+interface PageProps {
+  searchParams: {
+    [key: string]: string | string[] | undefined;
+  };
+}
+
+const page = async ({ searchParams }: PageProps) => {
+  const { id } = searchParams;
+  if (!id || typeof id !== "string") {
+    return notFound();
+  }
+
+  const configuration = await db.configuartion.findUnique({
+    where: { id },
+  });
+  if (!configuration) {
+    return notFound();
+  }
+
+  const { imageUrl, width, height } = configuration;
+
+  return (
+    <div>
+      <DesignConfigurator
+        imageUrl={imageUrl}
+        imageDimensions={{ width, height }}
+        configId={configuration.id}
+      />
+    </div>
+  );
+};
+
+export default page;
