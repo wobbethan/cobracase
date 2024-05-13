46d6deb652a708fe59edd380f924d7a88bcf77b7
wobbethan <wobbethan@gmail.com>
2024-05-12 21:17:45 -0400

    dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,5 +1,25 @@
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { notFound, redirect } from "next/navigation";
+import { db } from "../../db/index";
+import { formatPrice } from "../../lib/utils";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Progress } from "@/components/ui/progress";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import StatusDropdown from "./StatusDropdown";
 
 const Page = async () => {
   const { getUser } = getKindeServerSession();
@@ -8,7 +28,134 @@ const Page = async () => {
   const ADMIN_EMAIL = process.env.ADMIN_EMAIL;
 
   if (!user || user?.email !== ADMIN_EMAIL) return redirect("/");
-  return <div className="flex min-h-screen w-full bg-muted/40">Secret</div>;
+
+  const orders = await db.order.findMany({
+    where: {
+      isPaid: true,
+      createdAt: {
+        gte: new Date(new Date().setDate(new Date().getDate() - 7)),
+      },
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+    include: {
+      user: true,
+      shippingAddress: true,
+    },
+  });
+
+  const lastWeekSum = await db.order.aggregate({
+    where: {
+      isPaid: true,
+      createdAt: {
+        gte: new Date(new Date().setDate(new Date().getDate() - 7)),
+      },
+    },
+    _sum: {
+      amount: true,
+    },
+  });
+
+  const WEEKLY_GOAL = 500;
+
+  const lastMonthSum = await db.order.aggregate({
+    where: {
+      isPaid: true,
+      createdAt: {
+        gte: new Date(new Date().setDate(new Date().getDate() - 30)),
+      },
+    },
+    _sum: {
+      amount: true,
+    },
+  });
+
+  const MONTHLY_GOAL = 2500;
+
+  return (
+    <div className="flex min-h-screen w-full bg-muted/40">
+      <div className="max-w-7xl w-full mx-auto flex flex-col sm:gap-4 sm:py-4">
+        <div className="flex flex-col gap-16">
+          <div className="grid gap-4 sm:grid-cols-2">
+            <Card>
+              <CardHeader className="pb-2">
+                <CardDescription>Last Week</CardDescription>
+                <CardTitle className="text-4xl">
+                  {formatPrice(lastWeekSum._sum.amount ?? 0)}
+                </CardTitle>
+              </CardHeader>
+              <CardContent>
+                <div className="text-sm text-muted-foreground">
+                  of {formatPrice(WEEKLY_GOAL)}
+                </div>
+              </CardContent>
+              <CardFooter>
+                <Progress
+                  value={((lastWeekSum._sum.amount ?? 0) / WEEKLY_GOAL) * 100}
+                />
+              </CardFooter>
+            </Card>
+            <Card>
+              <CardHeader className="pb-2">
+                <CardDescription>Last Month</CardDescription>
+                <CardTitle className="text-4xl">
+                  {formatPrice(lastMonthSum._sum.amount ?? 0)}
+                </CardTitle>
+              </CardHeader>
+              <CardContent>
+                <div className="text-sm text-muted-foreground">
+                  of {formatPrice(MONTHLY_GOAL)}
+                </div>
+              </CardContent>
+              <CardFooter>
+                <Progress
+                  value={((lastMonthSum._sum.amount ?? 0) / MONTHLY_GOAL) * 100}
+                />
+              </CardFooter>
+            </Card>
+          </div>
+          <h1 className="text-4xl font-bold tracking-tight">Incoming orders</h1>
+          <Table>
+            <TableHeader>
+              <TableRow>
+                <TableHead>Customer</TableHead>
+                <TableHead className="hidden sm:table-cell">Status</TableHead>
+                <TableHead className="hidden sm:table-cell">
+                  Purchase Date
+                </TableHead>
+                <TableHead className="text-right">Amount</TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              {orders.map((order) => (
+                <TableRow key={order.id} className="bg-accent">
+                  <TableCell>
+                    <div className="font-medium">
+                      {order.shippingAddress?.name}
+                    </div>
+                    <div className="hidden text-wsm text-muted-foreground md:inline">
+                      {order.user.email}
+                    </div>
+                  </TableCell>
+                  <TableCell className="hidden sm:table-cell">
+                    {" "}
+                    <StatusDropdown id={order.id} orderStatus={order.status} />
+                  </TableCell>
+                  <TableCell className="hidden sm:table-cell">
+                    {order.createdAt.toLocaleDateString()}
+                  </TableCell>
+                  <TableCell className="text-right">
+                    {formatPrice(order.amount)}
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Page;
